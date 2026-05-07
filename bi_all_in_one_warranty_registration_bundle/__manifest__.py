0a43e2598326bc97efdfce6990f49a39478330ec
michaeldinhca <michaeldinhca@gmail.com>
2026-05-07 19:36:30 -0400

    Add files via upload

---
+++ bi_all_in_one_warranty_registration_bundle/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+	'name':"All in one Product Warranty Registration bundle",
+	'version':'19.0.0.0',
+	'category':'Industries',
+	'summary':'Website product Warranty with serial number claim Warranty point of sale Warranty all product Warranty register product Warranty claim Warranty Maintenance website warranty product service warranty Maintenance pos product warranty Registration pos warranty',
+	'description':"""
+
+	All in one Warranty Registration bundle in odoo,
+	Product Warranty Registration from POS and Warranty Renewal, Warranty Claim in odoo,
+	Product Warranty Registration, Warranty Renewal & Warranty Claim in Odoo,
+	Website Product Warranty Registration, Warranty Renewal & Warranty Claim in odoo,
+	Serial/Lot Number on Invoice/Sales With Scan feature in odoo,
+
+	""",
+	'author':"BROWSEINFO",
+    'website': 'https://www.browseinfo.com/demo-request?app=bi_all_in_one_warranty_registration_bundle&version=19&edition=Community',
+    "price": 10.0,
+    "currency": 'EUR',
+    'depends':['bi_pos_warranty','bi_warranty_registration',
+	'bi_website_warranty_registration','v9_sale_invoice_serial'],
+	'data':[],
+    'qweb': [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "license":'OPL-1',
+	'live_test_url': 'https://www.browseinfo.com/demo-request?app=bi_all_in_one_warranty_registration_bundle&version=19&edition=Community',
+	"images":["static/description/Banner.gif"],
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
